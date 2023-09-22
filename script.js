530ce3bb8ea9b6d4436efb70672dad36bde35c59
Pipe141 <144282971+Pipe141@users.noreply.github.com>
2023-09-22 18:52:10 -0500

    Add files via upload
    Portafolio Santiago Durán

---
+++ script.js
@@ -0,0 +1,12 @@
+let menuVisible = false
+// funcion que oculta/muestra el menu
+function mostrarOcultarMenu(){
+    if{menVisible}{
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    }
+    else{
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
